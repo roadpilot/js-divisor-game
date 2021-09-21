736275952ed5ac8f11892f69fd80649fc2957f82
Martin Achtermann <65299501+roadpilot@users.noreply.github.com>
2021-09-21 13:55:45 +0000

    solution

--- index.js
+++ index.js
@@ -3,12 +3,5 @@
  * @return {boolean}
  */
 var divisorGame = function(n) {
-    console.log(n)
-    let x = n-1
-    if (n % x == 0){
-        return true
-    }
-        else {
-            return divisorGame(x)
-        }
+    return n % 2 == 0
 };
